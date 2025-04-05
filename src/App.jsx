f6d0418c44e8149a26d662fd63e7021424588d51
renanmirandae <renanmirandae@gmail.com>
2025-04-05 18:26:19 -0300

    Criando a pagina principal da calculadora, inserindo botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { Button } from "./components/Button"
+import { useState } from "react";
+
 function App() {
 
+  //Array que armazena todos os operadores aritmeticos da calculadora
+  const operadoresAritmeticos = ["+", "-", "/", "*", "="];
+
+  //Array que armazena todos os numeros que serão utilizados na calculadora
+  const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  //Armazena os valores de cada botao incluindo os valores de operações
+  const [operacao, setOperacao] = useState("");
+
+  /*
+  Essa função captura o evento de click de cada botão, captura seu value e inseri na 
+  variavel de estado operacao
+  */
+  const handleButton = (e) => {
+    e.preventDefault();
+    setOperacao((prev => prev + e.target.value));
+  }
+
+
   return (
-    <>
-    <p>Ola mundo</p>
-    </>
+    <div className="container-calculadora">
+      <section className="box-tela">
+        <span>
+          {operacao}
+        </span>
+      </section>
+      <section className="box-numeros">
+        {/* Botões operador */}
+        {operadoresAritmeticos.map(item => <Button title={item} value={item} aoClicar={handleButton} />
+        )}
+
+        {/* Botões numéricos */}
+        {numeros.map(item => <Button title={item} value={item} aoClicar={handleButton} />)}
+      </section>
+    </div>
   )
 }
 
